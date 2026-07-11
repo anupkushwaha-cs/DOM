f7214f7dab240d9445d888ccb1e35004d3e45036
Anup Kushwaha <anupkushwaha916@gmail.com>
2026-07-11 16:30:36 +0530

    Day 1 DOM Practice

---
+++ Day 1/script.js
@@ -0,0 +1,14 @@
+let h1= document.querySelector("h1");
+let p= document.querySelector("p");
+let box= document.querySelector("#box");
+   
+   
+h1.innerHTML="Heyy";
+
+
+p.innerText= "hay I am Changed";
+
+
+box.style .backgroundColor= "red";
+box.style.padding= "40px";
+box.style.alignItems= "center";
