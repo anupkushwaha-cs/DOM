b0cdfc899cccd5610d14364c6dd7ea3509d5f3d5
Anup Kushwaha <anupkushwaha916@gmail.com>
2026-08-08 16:15:47 +0530

    Events Check

--- Day 4/Event/script.js
+++ Day 4/Event/script.js
@@ -1,7 +1,33 @@
-let btn = document.querySelector(".btn");
+// let btn = document.querySelector(".btn");
 
-btn.addEventListener("mouseenter", (event) =>{
-    console.log(event);
+// btn.addEventListener("mouseenter", (event) =>{
+//     console.log(event);
     
-}
-)
+// }
+// )
+const btn = document.querySelector(".btn");
+btn.addEventListener("click",  (event) =>{
+    console.log("button triggered...");
+    
+});
+
+const head = document.querySelector("head");
+btn.addEventListener("click",  (event) =>{
+    console.log("head triggered...");
+    
+});
+
+const div = document.querySelector("div");
+btn.addEventListener("click",  (event) =>{
+    console.log("div triggered...");
+    
+});
+
+
+
+const body = document.querySelector("body");
+btn.addEventListener("click",  (event) =>{
+    console.log("body triggered...");
+    
+});
+
