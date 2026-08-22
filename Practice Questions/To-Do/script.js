b7eab54350d38276a2f08807513653ca169d5664
Anup Kushwaha <anupkushwaha916@gmail.com>
2026-08-22 11:10:03 +0530

    meter

---
+++ Practice Questions/To-Do/script.js
@@ -0,0 +1,16 @@
+
+
+let input = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click", function (){   
+     if(input.value.trim()==="") return
+    let li = document.createElement("li");
+    li.innerText = input.value;
+
+    ul.appendChild(li)
+
+    input.value="";
+    
+});
